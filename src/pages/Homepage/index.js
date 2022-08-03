75d0442e5de9cf595e0c7b1ffdb030e5d8304c2c
Miriamvdb <miriamvdb@live.nl>
2022-08-03 10:17:26 +0200

    Feature 2: Select language - Work in progress...

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,8 +1,17 @@
-// import { RiArrowDropRightLine } from "react-icons/ri";
-// import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { RiArrowDropRightLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
 import "./styles.css";
 
 const Homepage = () => {
+  const [languages, setLanguages] = useState([
+    "English",
+    "Dutch",
+    "German",
+    "Spanish",
+  ]);
+  const [selectLanguage, setSelectLanguage] = useState("");
+
   return (
     <div className="container-homepage">
       <h1>Welcome to Real Estate</h1>
@@ -11,22 +20,34 @@ const Homepage = () => {
         alt="homepicture"
         style={{ width: "50%", marginBottom: "2rem" }}
       />
-      {/* <div className="container-buttons-homepage">
+      <div className="container-buttons-homepage">
         <div>
-          <NavLink className="navlink-style" to="/">
+          <NavLink className="navlink-style" to="/listings">
             <button className="button-homepage">
               See listings <RiArrowDropRightLine className="icon-homepage" />
             </button>
           </NavLink>
         </div>
         <div>
-          <NavLink className="navlink-style" to="/">
+          <NavLink className="navlink-style" to={`/aboutus/${languages}`}>
             <button className="button-homepage">
               About us <RiArrowDropRightLine className="icon-homepage" />
             </button>
           </NavLink>
         </div>
-      </div> */}
+        <div>
+          <form>
+            <select
+              value={selectLanguage}
+              onChange={(event) => setSelectLanguage(event.target.value)}
+            >
+              {languages.map((language, index) => {
+                return <option key={index}>{language}</option>;
+              })}
+            </select>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
